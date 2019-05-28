43b0450a6044ad52b6479d46cf90d0f3a137d62d
Chen Yu Pao <windperson@gmail.com>
2019-05-28 16:51:32 +0800

    CreateClient API change

--- src/GranDen.Orleans.Client.CommonLib/OrleansClientBuilder.cs
+++ src/GranDen.Orleans.Client.CommonLib/OrleansClientBuilder.cs
@@ -1,34 +1,46 @@
 ﻿using System;
 using System.Collections.Generic;
 using Microsoft.Extensions.Logging;
-using Orleans.Client.CommonLib.TypedOptions;
+using GranDen.Orleans.Client.CommonLib.TypedOptions;
 using Orleans.Configuration;
 using Serilog;
 using ILogger = Microsoft.Extensions.Logging.ILogger;
+using Orleans;
 
 namespace GranDen.Orleans.Client.CommonLib
 {
     public static class OrleansClientBuilder
     {
+        /// <summary>
+        /// Create Orleans Client using various configuration options
+        /// </summary>
+        /// <param name="logger">Logger to log ClientBuilder operation information</param>
+        /// <param name="clusterInfo"></param>
+        /// <param name="providerOption"></param>
+        /// <param name="applicationPartTypes">Application parts (optional)</param>
+        /// <param name="usingSerilog">Default use serilog, set to false to use Orleans' original logging.</param>
+        /// <returns></returns>
         public static IClusterClient CreateClient(ILogger logger,
             ClusterInfoOption clusterInfo,
             OrleansProviderOption providerOption,
-            IEnumerable<Type> applicationPartTypes)
+            IEnumerable<Type> applicationPartTypes = null,
+            bool usingSerilog = true)
         {
             var clientBuilder = new ClientBuilder();
 
             clientBuilder.Configure<ClientMessagingOptions>(options =>
             {
-                    options.ResponseTimeout = TimeSpan.FromSeconds(20);
-                    options.ResponseTimeoutWithDebugger = TimeSpan.FromMinutes(60);
+                options.ResponseTimeout = TimeSpan.FromSeconds(20);
+                options.ResponseTimeoutWithDebugger = TimeSpan.FromMinutes(60);
             }).Configure<ClusterOptions>(options =>
             {
-                    options.ClusterId = clusterInfo.ClusterId;
-                    options.ServiceId = clusterInfo.ServiceId;
+                options.ClusterId = clusterInfo.ClusterId;
+                options.ServiceId = clusterInfo.ServiceId;
             });
 
             if (providerOption.DefaultProvider == "MongoDB")
             {
+                logger.LogTrace("Using MongoDB provider...");
                 clientBuilder.UseMongoDBClustering(options =>
                 {
                     var mongoSetting = providerOption.MongoDB.Cluster;
@@ -40,13 +52,21 @@ namespace GranDen.Orleans.Client.CommonLib
                 });
             }
 
-            clientBuilder.ConfigureApplicationParts(manager =>
+            if(applicationPartTypes != null)
             {
-                foreach (var applicationPartType in applicationPartTypes)
+                clientBuilder.ConfigureApplicationParts(manager =>
                 {
-                    manager.AddApplicationPart(applicationPartType.Assembly).WithReferences();
-                }
-            }).ConfigureLogging(builder => { builder.AddSerilog(dispose: true); });
+                    foreach (var applicationPartType in applicationPartTypes)
+                    {
+                        manager.AddApplicationPart(applicationPartType.Assembly).WithReferences();
+                    }
+                });
+            }
+            
+            if(usingSerilog)
+            {
+                clientBuilder.ConfigureLogging(builder => { builder.AddSerilog(dispose: true); });
+            }
 
             try
             {
