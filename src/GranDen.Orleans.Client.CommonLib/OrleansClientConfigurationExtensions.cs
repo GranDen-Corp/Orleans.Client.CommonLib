81fc65ffbd7f742cf7e0e09b92c367c1d2e56b52
Chen Yu Pao <windperson@gmail.com>
2019-05-30 19:01:51 +0800

    enable source link

---
+++ src/GranDen.Orleans.Client.CommonLib/OrleansClientConfigurationExtensions.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using GranDen.Orleans.Client.CommonLib.TypedOptions;
+using Microsoft.Extensions.Configuration;
+
+namespace GranDen.Orleans.Client.CommonLib
+{
+    /// <summary>
+    /// Get Client configuration object utility
+    /// </summary>
+    public static class OrleansClientConfigurationExtensions
+    {
+        /// <summary>
+        /// Get client typed config object helper method
+        /// </summary>
+        /// <param name="configurationRoot"></param>
+        /// <param name="orleansSecetionKey"></param>
+        /// <param name="orleansSiloClusterSectionKey"></param>
+        /// <param name="orleansSiloProviderSectionKey"></param>
+        /// <returns></returns>
+        public static (ClusterInfoOption, OrleansProviderOption) GetSiloSettings(this IConfigurationRoot configurationRoot, 
+                                string orleansSecetionKey = "Orleans", 
+                                string orleansSiloClusterSectionKey = "Cluster", 
+                                string orleansSiloProviderSectionKey = "Provider")
+        {
+           var config = configurationRoot.GetSection(orleansSecetionKey);
+
+            var clusterInfo = new ClusterInfoOption();
+            config.GetSection(orleansSiloClusterSectionKey).Bind(clusterInfo);
+
+            var providerOption = new OrleansProviderOption();
+            config.GetSection(orleansSiloProviderSectionKey).Bind(providerOption);
+
+            return (clusterInfo, providerOption);
+        }
+    }
+}
--- src/GranDen.Orleans.Client.CommonLib/TypedOptions/ClientConnectOptions.cs
+++ src/GranDen.Orleans.Client.CommonLib/TypedOptions/ClientConnectOptions.cs
@@ -1,26 +1,45 @@
+﻿using System.ComponentModel.DataAnnotations;
+
 namespace GranDen.Orleans.Client.CommonLib.TypedOptions
 {
+    /// <summary>
+    /// Orleans Silo typed configuration class
+    /// </summary>
     public class ClusterInfoOption
     {
+        [Required]
         public string ClusterId { get; set; }
+        [Required]
         public string ServiceId { get; set; }
     }
 
+    /// <summary>
+    /// Default Storage Provider's name, currently only "MongoDB"
+    /// </summary>
     public class OrleansProviderOption
     {
+        [Required]
         public string DefaultProvider { get; set; }
 
         public MongoDbProviderSettings MongoDB { get; set; }
     }
 
+    /// <summary>
+    /// MongoDB Provider typed configuration class
+    /// </summary>
     public class MongoDbProviderSettings
     {
         public MongoDbProviderClusterSettings Cluster { get; set; }
     }
 
+    /// <summary>
+    /// Silo Cluster MongoDB Provider typed configuration class
+    /// </summary>
     public class MongoDbProviderClusterSettings
     {
+        [Required]
         public string DbConn { get; set; }
+        [Required]
         public string DbName { get; set; }
         public string CollectionPrefix { get; set; }
     }
