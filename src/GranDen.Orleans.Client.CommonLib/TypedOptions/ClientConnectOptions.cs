51149deb949cd1a4dbfde23cc2495d9c3c7c03f6
Chen Yu Pao <windperson@gmail.com>
2019-06-06 17:02:38 +0800

    v1.0.6

--- src/GranDen.Orleans.Client.CommonLib/TypedOptions/ClientConnectOptions.cs
+++ src/GranDen.Orleans.Client.CommonLib/TypedOptions/ClientConnectOptions.cs
@@ -7,8 +7,15 @@ namespace GranDen.Orleans.Client.CommonLib.TypedOptions
     /// </summary>
     public class ClusterInfoOption
     {
+        /// <summary>
+        /// Silo Cluster Id
+        /// </summary>
         [Required]
         public string ClusterId { get; set; }
+
+        /// <summary>
+        /// Silo Service Id
+        /// </summary>
         [Required]
         public string ServiceId { get; set; }
     }
@@ -18,9 +25,15 @@ namespace GranDen.Orleans.Client.CommonLib.TypedOptions
     /// </summary>
     public class OrleansProviderOption
     {
+        /// <summary>
+        /// Default Storage Provider's name, currently only "MongoDB"
+        /// </summary>
         [Required]
         public string DefaultProvider { get; set; }
 
+        /// <summary>
+        /// Top level MongoDB provider setting
+        /// </summary>
         public MongoDbProviderSettings MongoDB { get; set; }
     }
 
@@ -29,6 +42,9 @@ namespace GranDen.Orleans.Client.CommonLib.TypedOptions
     /// </summary>
     public class MongoDbProviderSettings
     {
+        /// <summary>
+        /// Silo Cluster MongoDB provider setting
+        /// </summary>
         public MongoDbProviderClusterSettings Cluster { get; set; }
     }
 
@@ -37,10 +53,21 @@ namespace GranDen.Orleans.Client.CommonLib.TypedOptions
     /// </summary>
     public class MongoDbProviderClusterSettings
     {
+        /// <summary>
+        /// MongoDB connection string
+        /// </summary>
         [Required]
         public string DbConn { get; set; }
+
+        /// <summary>
+        /// MongoDB database name
+        /// </summary>
         [Required]
         public string DbName { get; set; }
+
+        /// <summary>
+        /// Collection name prefix
+        /// </summary>
         public string CollectionPrefix { get; set; }
     }
 }
