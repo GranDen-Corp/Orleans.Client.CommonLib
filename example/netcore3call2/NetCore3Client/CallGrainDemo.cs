89e385cd65f864cbff0253f8150f3942e4d83af1
Chen Yu Pao <windperson@gmail.com>
2019-12-18 16:01:44 +0800

    Add new client call old server test

---
+++ example/netcore3call2/NetCore3Client/CallGrainDemo.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading.Tasks;
+using GranDen.Orleans.Client.CommonLib;
+using HelloNetStandard2_1.ShareInterface;
+using Microsoft.Extensions.Logging;
+
+namespace NetCore3Client
+{
+    class CallGrainDemo
+    {
+        private readonly ILogger<CallGrainDemo> _logger;
+
+        public CallGrainDemo(ILogger<CallGrainDemo> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task DemoRun()
+        {
+            using var client = OrleansClientBuilder.CreateLocalhostClient(_logger, clusterId: "dev", serviceId: "HelloWorldApp");
+            await client.ConnectWithRetryAsync();
+            _logger.LogInformation("Client successfully connect to silo host");
+
+            var grain = client.GetGrain<IHello>(0);
+            _logger.LogInformation("Get hello world grain, start calling RPC methods...");
+
+            var returnValue = await grain.SayHello("Hello Orleans");
+            _logger.LogInformation($"RPC method return value is \r\n\r\n{{{returnValue}}}\r\n");
+
+            await client.Close();
+            _logger.LogInformation("Client successfully close connection to silo host");
+        }
+    }
+}
---
+++ example/netcore3call2/backend/NetCore2Demo.Grain/HelloGrain.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading.Tasks;
+using HelloNetStandard2_1.ShareInterface;
+using Microsoft.Extensions.Logging;
+
+namespace NetCore3Demo.Grain
+{
+    public class HelloGrain : Orleans.Grain, IHello
+    {
+        private readonly ILogger<HelloGrain> _logger;
+
+        public HelloGrain(ILogger<HelloGrain> logger)
+        {
+            _logger = logger;
+        }
+
+        public Task<string> SayHello(string greeting)
+        {
+            _logger.LogInformation($"SayHello message received: greeting = '{greeting}'");
+            return Task.FromResult($"You said: '{greeting}', I say: Hello!");
+        }
+    }
+}
